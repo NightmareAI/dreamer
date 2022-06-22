87f428b5bf06f74ed0f77fc086fb102c31a5ba86
Stephan Auerhahn <palp@palp.org>
2022-06-21 19:38:51 -0700

    do that

--- dreamer.py
+++ dreamer.py
@@ -146,15 +146,16 @@ def realesrgan_replicate(id: str, scale: int, face_enhance: bool):
     if not os.path.exists("/result"):
         os.makedirs("/result")
     model = replicate.models.get("nightmareai/real-esrgan")
-    result = model.predict(
+    for result in model.predict(
         image=open("/input/lq/input.png", "rb"),
         scale=scale,
         face_enhance=face_enhance,
-    )
-    with requests.get(result, stream=True) as r:
-        with open("/result/output.png", "wb") as f:
-            for chunk in r.iter_content(chunk_size=16 * 1024):
-                f.write(chunk)
+    ):
+        with requests.get(result, stream=True) as r:
+            with open("/result/output.png", "wb") as f:
+                for chunk in r.iter_content(chunk_size=16 * 1024):
+                    f.write(chunk)
+            break
 
 
 def pixray_replicate(id: str):
