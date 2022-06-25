edc7d345a838c98e6c19dcfc2b02489c34a41e80
Stephan Auerhahn <palp@palp.org>
2022-06-25 03:07:20 -0700

    bump publish image

--- dreamer.py
+++ dreamer.py
@@ -16,7 +16,7 @@ from dapr.clients import DaprClient
 from dapr.ext.grpc import App
 
 
-publish_image = "ghcr.io/nightmareai/nightmarebot-publish@sha256:2159ed6bf8886efcbe1583d6c41bc7e1dd65cbafa9239482636854a290d9ef15"
+publish_image = "ghcr.io/nightmareai/nightmarebot-publish@sha256:7dcc7b4e5dd852ae2430f89a16335cb435f9617b14b2edf35cdde2fa0720b1c6"
 majesty_image = "ghcr.io/nightmareai/majesty-diffusion@sha256:fb81446fcc13fa1a048c3d683e38113cae114aa5216218a639c03946b1f88dcb"
 pixray_image = "ghcr.io/nightmareai/pixray@sha256:4789d76512bbe483ef0c50023366f684e8f8775f59bf133f687761d3913e09e0"
 latent_diffusion_image = "ghcr.io/nightmareai/latent-diffusion@sha256:fc529d49066fc2d2764c42a66dd9e68a698cd9f67c76e262740edbfa9f8ca914"
